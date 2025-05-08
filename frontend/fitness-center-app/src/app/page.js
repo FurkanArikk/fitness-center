a6822ade41215e291579cc60a0fb38bf74149ff4
helinaslanoglu <helinaslanoglu@stu.aydin.edu.tr>
2025-05-08 15:57:33 +0300

    init commit for frontend

---
+++ frontend/fitness-center-app/src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      ana sayfa
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/fitness-center-app/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="bg-slate-900 text-white p-4">burası navbar</div>;
+};
+
+export default Navbar;
