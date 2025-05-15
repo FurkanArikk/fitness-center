d31dab6633e69d412267cf9cedd7558ba3b4f897
helinaslanoglu <helinaslanoglu@stu.aydin.edu.tr>
2025-05-15 15:56:37 +0300

    create welcome page

--- frontend/fitness-center-app/src/layouts/MainLayout.jsx
+++ frontend/fitness-center-app/src/layouts/MainLayout.jsx
@@ -1,34 +1,47 @@
 "use client";
 
-import React, { useState } from 'react';
-import Sidebar from '../components/common/Sidebar';
-import Navbar from '../components/common/Navbar';
+import React, { useState, useEffect } from "react";
+import Sidebar from "../components/common/Sidebar";
+import Navbar from "../components/common/Navbar";
+import { usePathname } from "next/navigation";
 
 const MainLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [currentPath, setCurrentPath] = useState("");
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname) {
+      setCurrentPath(pathname);
+    }
+  }, [pathname]);
+
+  const isWelcomePage = currentPath === "/welcome";
 
   return (
     <div className="min-h-screen bg-gray-50 flex">
       {/* Sidebar */}
-      <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
-      
+      {!isWelcomePage && (
+        <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+      )}
+
       {/* Overlay when sidebar is open on mobile */}
-      {sidebarOpen && (
-        <div 
+      {!isWelcomePage && sidebarOpen && (
+        <div
           className="fixed inset-0 bg-black bg-opacity-50 z-10 lg:hidden"
           onClick={() => setSidebarOpen(false)}
         ></div>
       )}
-      
+
       {/* Main content */}
       <main className="flex-1 overflow-y-auto">
         {/* Top navigation */}
-        <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-        
+        {!isWelcomePage && (
+          <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+        )}
+
         {/* Page content */}
-        <div className="p-6">
-          {children}
-        </div>
+        <div className="p-6">{children}</div>
       </main>
     </div>
   );
